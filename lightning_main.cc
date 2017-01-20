f71bb50405c89a05c58fec8da2b1b427037aad71
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2017-01-19 20:33:02 -0800

    Modify Lightning driver to verify args count

--- lightning_main.cc
+++ lightning_main.cc
@@ -1,14 +1,12 @@
+#include "config_parser.h"
+
 #include <iostream>
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
-    std::cerr << "I am only supposed to take ONE argument!" << std::endl;
-    std::cerr << "But you gave me " << argc
-              << " argument(s): \n" << std::endl;
-
-    for (int i = 0; i < argc; i++) {
-      std::cerr << "Argument " << i << ": "
-                << argv[i] << std::endl;
-    }
+    std::cout << "Usage: ./config_parser <path to config file>\n" << std::endl;
+    return 1;
   }
+
+  return 0;
 }
