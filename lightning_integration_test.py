10482a92595e9109e55b0c19f6000e117e02da76
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2017-01-23 13:24:38 -0800

    Remove unnecessary Lightning run from integration test

--- lightning_integration_test.py
+++ lightning_integration_test.py
@@ -7,8 +7,7 @@ print("START Lightning integration tests\n")
 # make -s suppresses output of commands as make executes them
 if (
     call(['make', '-s', 'clean']) != 0 or
-    call(['make', '-s']) != 0 or
-    call(['./lightning', 'simple_config']) != 0
+    call(['make', '-s']) != 0
    ):
     print('FAILED: Build and run with simple_config failed!')
 
