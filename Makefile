fbfd907c661ec40f8bb5c172f425db40d977ac79
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2017-01-23 13:15:05 -0800

    Set up initial integration test

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
 # Based on example Makefile:
 # https://www.cs.swarthmore.edu/~newhall/unixhelp/howto_makefiles.html
+
+# TODO: CXX is a predefined variable. What is its default value on different platforms?
+# TODO: MAKEOPTS += 2 (to parallelize building with 2 commands at once)
 CXX = g++
-SRC_FLAGS = -std=c++0x -I nginx-configparser/
+SRC_FLAGS = -std=c++0x -I nginx-configparser/ -Wall -Wextra
+# TODO: This should be named LDFLAGS for consistency with the Makefile community
 LIB_FLAGS = -lpthread -lboost_system
 PARSER_PATH = ./nginx-configparser/
+
+# TODO: Make a phony, so that targets are not treated as files on-disk
+# TODO: Split out dependency handling so that we can avoid rebuilding
 
 TARGET = lightning
 SRC = $(PARSER_PATH)config_parser.cc lightning_main.cc lightning_server.cc
--- lightning_integration_test.py
+++ lightning_integration_test.py
@@ -1,14 +1,37 @@
 #!/usr/bin/env python
 
-from subprocess import call
+import subprocess
+from subprocess import run
 
 print("START Lightning integration tests\n")
 
 # make -s suppresses output of commands as make executes them
+# subprocess.run returns a CompletedProcess object
 if (
-    call(['make', '-s', 'clean']) != 0 or
-    call(['make', '-s']) != 0
+    run(['make', '-s', 'clean']).returncode != 0 or
+    run(['make', '-s']).returncode != 0
    ):
-    print('FAILED: Build and run with simple_config failed!')
+    print('FAILED: Build with make failed!')
+
+# Test echoing
+# Try a background fork/thread
+if (run(['./lightning', 'simple_config']).returncode != 0):
+    print('FAILED: Lightning server failed to start with simple config')
+
+# TODO: Have intermediate logging throughout
+# TODO: Use Python unit test frameworks + logging libraries
+print("After server start")
+
+# TODO: HTTPie is failing to run
+original_request = 'GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: HTTPie/0.9.8\r\nAccept-Encoding: gzip, deflate\r\nAccept: */*\r\nConnection: keep-alive\r\n\r\n'
+completed_request = run(['http', 'localhost:8080'], stdout=subprocess.PIPE)
+
+if (completed_request.returncode != 0 or
+    completed_request.stdout != original_request
+   ):
+    print('FAILED: httpie received a non-matching echo response')
+
+# Terminate the server
+run(['pkill', 'lightning'])
 
 print("\nEND Lightning integration tests. All tests passed!")
