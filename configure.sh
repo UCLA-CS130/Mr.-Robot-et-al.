387b4db092747381e1210d1e764fa1812faa4d33
Frank Chen <frankchen1698@gmail.com>
2017-01-31 12:34:07 -0800

    Add -y flag to install without prompt

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
-sudo add-apt-repository ppa:jonathonf/python-3.6
-sudo apt-get update
-sudo apt-get install python3.6
+sudo add-apt-repository -y ppa:jonathonf/python-3.6
+sudo apt-get -y update
+sudo apt-get -y install python3.6
 PATH=$(which python3.6)
 ln -s $PATH /usr/local/bin/python3.6
