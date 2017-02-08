d51a591b0357f081f06c90e0d9fdcef279bb2440
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2017-02-08 10:20:29 -0800

    Clean up request-handler tests
    * Remove outdated comments and TODOs
    * Remove commented code
    * Use const where possible
    * Standardize upon strlen() for finding expected lengths
    * Prevent memory leak with dynamically allocated
      expected_response_buffer

--- request_handlers_test.cc
+++ request_handlers_test.cc
@@ -31,15 +31,6 @@ protected:
                                request_buffer_size,
                                response_buffer,
                                response_buffer_size);
-
-    /*
-    if (response_buffer == nullptr) {
-      return false;
-    }
-    else {
-      return true;
-    }
-    */
   }
 };
 
@@ -48,7 +39,7 @@ protected:
 // invalid file
 
 TEST_F(RequestHandlersTest, EmptyStringTest) {
-  char request_buffer[] = "";
+  const char request_buffer[] = "";
   const size_t request_buffer_size = 0;
 
   // The request_buffer is normally filled by a socket-read
@@ -64,7 +55,6 @@ TEST_F(RequestHandlersTest, EmptyStringTest) {
                              response_buffer,
                              response_buffer_size));
 
-
   EXPECT_EQ(response_buffer_size, expected_response_buffer_size)
     << "Response size should only be header size";
   EXPECT_STREQ(response_buffer, NULL)
@@ -72,13 +62,12 @@ TEST_F(RequestHandlersTest, EmptyStringTest) {
 }
 
 TEST_F(RequestHandlersTest, RandomStringTest) {
-  char request_buffer[] = "abcd\n";
+  const char request_buffer[] = "abcd\n";
   const size_t request_buffer_size = 5;
 
   char* response_buffer = nullptr;
   size_t response_buffer_size = 0;
 
-  // Variable for comparing with actual response_buffer
   const char expected_response_buffer[] = "";
   const size_t expected_response_buffer_size = 0;
 
@@ -101,9 +90,8 @@ TEST_F(RequestHandlersTest, OutputWithHeader) {
     "text/html,application/xhtml+xml,application/xml;"
     "q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n"
     "Accept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\n\r\n";
-  const size_t request_buffer_size = 296;
+  const size_t request_buffer_size = strlen(request_buffer);
 
-  // Handle echo response in external handler
   char* response_buffer = nullptr;
   size_t response_buffer_size = 0;
 
@@ -116,7 +104,7 @@ TEST_F(RequestHandlersTest, OutputWithHeader) {
     "text/html,application/xhtml+xml,application/xml;"
     "q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n"
     "Accept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\n\r\n";
-  const size_t expected_response_buffer_size = ok_header_size + request_buffer_size;
+  const size_t expected_response_buffer_size = strlen(test_response_buffer);
 
   ASSERT_TRUE(HandleRequest(request_buffer,
                             request_buffer_size,
@@ -130,33 +118,20 @@ TEST_F(RequestHandlersTest, OutputWithHeader) {
 }
 
 TEST_F(RequestHandlersTest, HTMLTest) {
-  char request_buffer[] = "GET /static1/index.html HTTP/1.1\r\n"
+  const char request_buffer[] = "GET /static1/index.html HTTP/1.1\r\n"
                           "Host: localhost:8080\r\n"
                           "Accept-Encoding: gzip, deflate, compress\r\n"
                           "Accept: */*\r\n"
                           "User-Agent: HTTPie/0.8.0\r\n\r\n";
-
   const size_t request_buffer_size = strlen(request_buffer);
 
-  // The request_buffer is normally filled by a socket-read
-  // The response_buffer is allocated by the request-handler
   char* response_buffer = nullptr;
   size_t response_buffer_size = 0;
 
   const char expected_response_buffer[] =
     "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE html>\n<html>\n"
     "  <head></head>\n  <body>\n    <h1>Hello World!</h1>\n  </body>\n</html>";
-  const size_t expected_response_buffer_size = 135;
-
-  // TODO: Need a way to diff two HTML files
-  // TODO: Need to convert string to char array to diff
-  /*
-  std::ifstream file("test/index.html");
-  std::stringstream buffer;
-  buffer << file.rdbuf();
-  std::string contents(buffer.str());
-  std::cout << contents << std::endl;
-  */
+  const size_t expected_response_buffer_size = strlen(expected_response_buffer);
 
   ASSERT_TRUE(HandleRequest(request_buffer,
                             request_buffer_size,
@@ -169,17 +144,14 @@ TEST_F(RequestHandlersTest, HTMLTest) {
     << "response_buffer should be the same string as test_response_buffer";
 }
 
-TEST_F(RequestHandlersTest, RouteDoesntExist) {
-  char request_buffer[] = "GET /nowhere HTTP/1.1\r\n"
+TEST_F(RequestHandlersTest, RouteDoesNotExist) {
+  const char request_buffer[] = "GET /nowhere HTTP/1.1\r\n"
                           "Host: localhost:8080\r\n"
                           "Accept-Encoding: gzip, deflate, compress\r\n"
                           "Accept: */*\r\n"
                           "User-Agent: HTTPie/0.8.0\r\n\r\n";
-
   const size_t request_buffer_size = strlen(request_buffer);
 
-  // The request_buffer is normally filled by a socket-read
-  // The response_buffer is allocated by the request-handler
   char* response_buffer = nullptr;
   size_t response_buffer_size = 0;
 
@@ -207,17 +179,14 @@ TEST_F(RequestHandlersTest, RouteDoesntExist) {
     << "response_buffer should be the same string as test_response_buffer";
 }
 
-TEST_F(RequestHandlersTest, RequestedFileDoesntExist) {
-  char request_buffer[] = "GET /static1/nonexistantfile HTTP/1.1\r\n"
+TEST_F(RequestHandlersTest, RequestedFileDoesNotExist) {
+  const char request_buffer[] = "GET /static1/nonexistantfile HTTP/1.1\r\n"
                           "Host: localhost:8080\r\n"
                           "Accept-Encoding: gzip, deflate, compress\r\n"
                           "Accept: */*\r\n"
                           "User-Agent: HTTPie/0.8.0\r\n\r\n";
-
   const size_t request_buffer_size = strlen(request_buffer);
 
-  // The request_buffer is normally filled by a socket-read
-  // The response_buffer is allocated by the request-handler
   char* response_buffer = nullptr;
   size_t response_buffer_size = 0;
 
@@ -231,7 +200,6 @@ TEST_F(RequestHandlersTest, RequestedFileDoesntExist) {
     "<h1>404 Page Not Found</h1>\n\n"
     "</head>\n"
     "</html>";
-
   const size_t expected_response_buffer_size = strlen(expected_response_buffer);
 
   ASSERT_FALSE(HandleRequest(request_buffer,
@@ -246,31 +214,24 @@ TEST_F(RequestHandlersTest, RequestedFileDoesntExist) {
 }
 
 TEST_F(RequestHandlersTest, PNGTest) {
-  char request_buffer[] = "GET /static1/angrybird.png HTTP/1.1\r\n"
+  const char request_buffer[] = "GET /static1/angrybird.png HTTP/1.1\r\n"
                           "Host: localhost:8080\r\n"
                           "Accept-Encoding: gzip, deflate, compress\r\n"
                           "Accept: */*\r\n"
                           "User-Agent: HTTPie/0.8.0\r\n\r\n";
-
   const size_t request_buffer_size = strlen(request_buffer);
 
-  // The request_buffer is normally filled by a socket-read
-  // The response_buffer is allocated by the request-handler
   char* response_buffer = nullptr;
   size_t response_buffer_size = 0;
 
-  // TODO: Need a way to diff two images; most online resources
-  // say to use OpenCV
-  // TODO: Need to convert string to char array to diff
   std::ifstream file("test/angrybird.png", std::ios::binary);
   std::ostringstream buffer;
   buffer << file.rdbuf();
   std::string image_content(buffer.str());
-  // std::cout << image_content << std::endl;
 
-  std::string content = 
-  "HTTP/1.1 200 OK\r\nContent-Type: image/png\r\n\r\n" + image_content;
-  size_t expected_response_buffer_size = content.size();
+  std::string content =
+    "HTTP/1.1 200 OK\r\nContent-Type: image/png\r\n\r\n" + image_content;
+  const size_t expected_response_buffer_size = content.size();
 
   char* expected_response_buffer = new char[expected_response_buffer_size];
   content.copy(expected_response_buffer, expected_response_buffer_size);
@@ -284,10 +245,12 @@ TEST_F(RequestHandlersTest, PNGTest) {
     << "Response size should be initial request buffer size plus header size";
   EXPECT_STREQ(expected_response_buffer, response_buffer)
     << "response_buffer should be the same string as test_response_buffer";
+
+  delete[] expected_response_buffer;
 }
 
 // TEST_F(RequestHandlersTest, GIFTest) {
-//   char request_buffer[] = "";
+//   const char request_buffer[] = "";
 //   const size_t request_buffer_size = 0;
 
 //   // The request_buffer is normally filled by a socket-read
