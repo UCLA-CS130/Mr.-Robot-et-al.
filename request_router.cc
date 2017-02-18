fe994f03b285b2c4303f69e621c3790d968817ee
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2017-02-18 15:45:58 -0800

    Implement RequestRouter::routeRequest with longest-prefix matching

--- request_router.cc
+++ request_router.cc
@@ -1,10 +1,43 @@
 #include "request_handlers.h"
 #include "request_router.h"
 
+typedef std::map<const std::string, const std::unique_ptr<RequestHandler>> HandlersMap;
+
 RequestRouter::RequestRouter(const ServerConfig& server_config) {
   // TODO
 }
 
-std::unique_ptr<RequestHandler> RequestRouter::routeRequest(const std::string& full_uri) {
-  // TODO
+std::unique_ptr<RequestHandler> RequestRouter::routeRequest(const std::string& full_uri) const {
+  // Starting at the end of the string,
+  // try longest-prefix match up to latest forward-slash
+  //
+  // Example: for the full_uri '/echo/subaction/subdir/file.txt',
+  // we try '/echo/subaction/subdir', then '/echo/subaction',
+  // and finally '/echo'.
+  //
+  // Note the lack of trailing slashes
+  // See also: http://www.cplusplus.com/reference/string/string/find_first_of/
+  for (int search_index = full_uri.size() - 1; search_index >= 0; search_index--) {
+    size_t pos = full_uri.find_first_of('/', search_index);
+    if (pos == std::string::npos) {
+      continue;
+    }
+    else {
+      // Slice string from 0 to pos; substr takes start and length
+      // TODO: off-by-one?
+      std::string possible_prefix = full_uri.substr(0, full_uri.size() - pos);
+      std::cerr << "RequestRouter::routeRequest prefix substring" << possible_prefix << std::endl;
+
+      HandlersMap::const_iterator it = handlers_map_.find(possible_prefix);
+      if (it == handlers_map_.end()) {
+        continue;
+      }
+      else {
+        // Found the matching longest-prefix; return corresponding handler
+        return std::move(it->second);
+      }
+    }
+  }
+
+  // TODO: If no prefix found, return 404 handler
 }
--- request_router.h
+++ request_router.h
@@ -5,6 +5,7 @@
 #include "server_config.h"
 
 #include <memory>
+#include <map>
 
 // Stores the mapping of (uri_prefix -> RequestHandler),
 // which we call a "route".
@@ -23,6 +24,7 @@ class RequestRouter {
     //          /static1 -> StaticHandler
     //          /static1/subaction -> DynamicServeHandler
     //
+    // Notice the lack of trailing slash: /prefix, not /prefix/
     // And each handler would store their uri_prefix + the NginxConfig
     // child-block corresponding to that path.
     RequestRouter(const ServerConfig& server_config);
@@ -37,7 +39,15 @@ class RequestRouter {
     //
     // would give back DynamicServeHandler, not StaticHandler,
     // as /static1/subaction is the longest-prefix that matches.
-    std::unique_ptr<RequestHandler> routeRequest(const std::string& full_uri);
+    std::unique_ptr<RequestHandler> routeRequest(const std::string& full_uri) const;
+
+  private:
+    // Map URI prefixes to request handlers
+    // TODO: Creating a unique_ptr to a polymorphic type in an <unordered_map>
+    // requires a specialization of std::hash. We can use <map> for now
+    // See: https://stackoverflow.com/questions/20965200/stdhash-for-unique-ptr-in-unordered-map
+    std::map<const std::string, const std::unique_ptr<RequestHandler>>
+      handlers_map_;
 };
 
 #endif
