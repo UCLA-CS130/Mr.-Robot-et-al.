cc20a7c95a93e9677829712351d48f7e142c7e0b
Frank Chen <frankchen1698@gmail.com>
2017-02-18 11:59:11 -0800

    Create request.h class

---
+++ request.h
@@ -0,0 +1,35 @@
+#ifndef REQUEST_H
+#define REQUEST_H
+
+#include <memory>
+#include <string>
+
+// Represents an HTTP Request.
+//
+// Usage:
+//   auto request = Request::Parse(raw_request);
+class Request {
+ public:
+  // raw_request is the string read from socket
+  static unique_ptr<Request> Parse(const std::string& raw_request);
+
+  std::string raw_request() const;
+  std::string method() const;
+  std::string uri() const;
+  std::string version() const;
+
+  using Headers = std::vector<std::pair<std::string, std::string>>;
+  Headers headers() const;
+
+  std::string body() const;
+
+ private:
+  std::string raw_request_;
+  std::string method_;
+  std::string uri_;
+  std::string version_;
+  Headers headers_;
+  std::string body_;
+};
+
+#endif
