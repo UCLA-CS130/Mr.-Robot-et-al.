c1d508aa32e05268e4c06c8e01209c7bab2a7e18
Alexander Fong <alpfong@gmail.com>
2017-02-21 01:22:55 -0800

    Refactor ServerConfig tests to support new API

--- server_config_test.cc
+++ server_config_test.cc
@@ -5,6 +5,7 @@
 
 #include <string>
 #include <sstream>
+#include <unordered_map>
 
 class ServerConfigTest : public ::testing::Test {
 protected:
@@ -20,74 +21,142 @@ protected:
   NginxConfig out_config_;
 };
 
+TEST_F(ServerConfigTest, BuildOK) {
+    std::unique_ptr<ServerConfig> server_config
+      = ParseString("port 8080; \
+                     path /echo EchoHandler {}");
+    bool ec = server_config->Build();
+
+    EXPECT_EQ(true, ec)
+      << "Build() should return true if a valid config is supplied to a \
+          Server Config is supplied";
+}
+
+TEST_F(ServerConfigTest, BuildFailed) {
+    std::unique_ptr<ServerConfig> server_config = ParseString("");
+    bool ec = server_config->Build();
+
+    EXPECT_EQ(false, ec)
+      << "Build() should return false if an empty config is supplied";
+}
+
+TEST_F(ServerConfigTest, AllPathsNotEmpty) {
+    std::unique_ptr<ServerConfig> server_config
+      = ParseString("port 8080; \
+                     path /echo EchoHandler {} \
+                     default NotFoundHandler {}");
+    bool ec = server_config->Build();
+    std::unordered_map<std::string, std::string> paths = server_config->allPaths();
+
+    EXPECT_EQ(true, ec)
+      << "Build() should return true if a valid config is supplied";
+    EXPECT_EQ(false, paths.empty());
+      << "allPaths() shouldn't return an empty map";
+}
+
+TEST_F(ServerConfigTest, AllPathsEmpty) {
+    std::unique_ptr<ServerConfig> server_config = ParseString("port 8080;");
+    bool ec = server_config->Build();
+    std::unordered_map<std::string, std::string> paths = server_config->allPaths();
+
+    EXPECT_EQ(true, ec)
+      << "Build() should return true if a valid config is supplied";
+    EXPECT_EQ(true, paths.empty())
+      << "allPaths() should return an empty map";
+}
+
 TEST_F(ServerConfigTest, NoPortConfig) {
     std::unique_ptr<ServerConfig> server_config = ParseString("server { ACM Hack; }");
-    std::vector<std::string> query = {"server", "listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(1, ec)
-      << "Error value of 1 should be returned if property isn't found";
+    std::vector<std::string> query = {"server", "port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(false, ec)
+      << "LookUp() should return false if a property isn't found";
     EXPECT_EQ("", port)
-      << "No port should be found without a 'listen' property";
+      << "No port should be found without a 'port' property";
 }
 
 TEST_F(ServerConfigTest, PortNumberMissing) {
-    std::unique_ptr<ServerConfig> server_config = ParseString("listen;");
-    std::vector<std::string> query = {"server", "listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+    std::unique_ptr<ServerConfig> server_config = ParseString("port;");
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(1, ec)
-      << "Error value of 1 should be returned if property isn't found";
+    std::vector<std::string> query = {"port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(false, buildCode)
+      << "Build() should return false when given a config property has no value.";
+    EXPECT_EQ(false, ec)
+      << "LookUp() should return false if a property doesn't have a value";
     EXPECT_EQ("", port)
       << "A listen property without a port number should not return a port";
 }
 
 TEST_F(ServerConfigTest, PortPropertyOnly) {
-    std::unique_ptr<ServerConfig> server_config = ParseString("listen 1234;");
-    std::vector<std::string> query = {"listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+    std::unique_ptr<ServerConfig> server_config = ParseString("port 1234;");
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(0, ec)
-      << "Error value of 0 should be returned if property is found";
+    std::vector<std::string> query = {"port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(true, ec)
+      << "LookUp() should return true if property is found";
     EXPECT_EQ("1234", port)
       << "A listen-portnumber pair should yield a port number";
 }
 
 TEST_F(ServerConfigTest, NestedPortNumberMissing) {
-    std::unique_ptr<ServerConfig> server_config = ParseString("server { listen; }");
-    std::vector<std::string> query = {"server", "listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+    std::unique_ptr<ServerConfig> server_config = ParseString("server { port; }");
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(1, ec)
-      << "Error value of 1 should be returned if property isn't found with a value";
+    std::vector<std::string> query = {"server", "port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(false, buildCode)
+      << "Build() should return false when given a property has no value";
+    EXPECT_EQ(false, ec)
+      << "LookUp() should return false if a nested property doesn't have a value";
     EXPECT_EQ("", port)
       << "A listen property without a port number in a nested block should \
           give an empty lookup result";
 }
 
-TEST_F(ServerConfigTest, NonsenseProperty) {
-    std::unique_ptr<ServerConfig> server_config = ParseString("listen 1234;");
+TEST_F(ServerConfigTest, NonsenseQueryPath) {
+    std::unique_ptr<ServerConfig> server_config = ParseString("port 1234;");
+    bool buildCode = server_config->Build();
+
     std::vector<std::string> query = {"laksdj3"};
     std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+    bool ec = server_config->propertyLookUp(query, port);
 
-    EXPECT_EQ(1, ec)
-      << "A property lookup on a nonsense property should return 1 (meaning error)";
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(false, ec)
+      << "A property lookup on a nonsense property should return false";
 }
 
 TEST_F(ServerConfigTest, SingleNestedPort) {
     std::unique_ptr<ServerConfig> server_config
-      = ParseString("server { listen 1234; }");
-    std::vector<std::string> query = {"server", "listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+      = ParseString("server { port 1234; }");
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(0, ec)
-      << "Error value of 0 should be returned if property is found";
+    std::vector<std::string> query = {"server", "port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(true, ec)
+      << "LookUp() should return true if property is found";
     EXPECT_EQ("1234", port)
       << "A listen property with a port number in a nested block should \
           yield a port number";
@@ -95,13 +164,17 @@ TEST_F(ServerConfigTest, SingleNestedPort) {
 
 TEST_F(ServerConfigTest, DoublyNestedPort) {
     std::unique_ptr<ServerConfig> server_config
-      = ParseString("outer { server { listen 1234; } }");
-    std::vector<std::string> query = {"outer", "server", "listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+      = ParseString("outer { server { port 1234; } }");
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(0, ec)
-      << "Error value of 0 should be returned if property is found";
+    std::vector<std::string> query = {"outer", "server", "port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(true, ec)
+      << "LookUp() should return true if property is found";
     EXPECT_EQ("1234", port)
       << "A listen property with a port number in a doubly nested block \
           should yield a port number";
@@ -109,13 +182,18 @@ TEST_F(ServerConfigTest, DoublyNestedPort) {
 
 TEST_F(ServerConfigTest, NonPortPropertiesPresent) {
     std::unique_ptr<ServerConfig> server_config
-      = ParseString("server { echo true; listen 1234; }");
-    std::vector<std::string> query = {"server", "listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+      = ParseString("server { echo true; port 1234; }");
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(0, ec)
-      << "Error value of 0 should be returned if property is found";
+    std::vector<std::string> query = {"server", "port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(true, ec)
+      << "LookUp() should return true if property is found, even if it's not \
+          the first property in a block";
     EXPECT_EQ("1234", port)
       << "A listen property with a port number in a doubly nested block with \
           other properties should yield a port number";
@@ -123,13 +201,17 @@ TEST_F(ServerConfigTest, NonPortPropertiesPresent) {
 
 TEST_F(ServerConfigTest, HighlyNestedPropertiesSet) {
     std::unique_ptr<ServerConfig> server_config
-      = ParseString("outer { server { echo true; listen 1234; } }");
-    std::vector<std::string> query = {"outer", "server", "listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+      = ParseString("outer { server { echo true; port 1234; } }");
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(0, ec)
-      << "Error value of 0 should be returned if property is found";
+    std::vector<std::string> query = {"outer", "server", "port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(true, ec)
+      << "LookUp() should return true if property is found";
     EXPECT_EQ("1234", port)
       << "A listen property with a port number in a triply nested block with \
           other properties should yield a port number";
@@ -137,13 +219,17 @@ TEST_F(ServerConfigTest, HighlyNestedPropertiesSet) {
 
 TEST_F(ServerConfigTest, PropertiesSetWithNewlines) {
     std::unique_ptr<ServerConfig> server_config
-      = ParseString("server { echo \ntrue; listen \n1234; }");
-    std::vector<std::string> query = {"server", "listen"};
-    std::string port = "";
-    int ec = server_config->propertyLookUp(query, port);
+      = ParseString("server { echo \ntrue; port \n1234; }");
+    bool buildCode = server_config->Build();
 
-    EXPECT_EQ(0, ec)
-      << "Error value of 0 should be returned if property is found";
+    std::vector<std::string> query = {"server", "port"};
+    std::string port = "";
+    bool ec = server_config->propertyLookUp(query, port);
+
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(true, ec)
+      << "LookUp() should return true if property is found";
     EXPECT_EQ("1234", port)
       << "A listen property with a port number in a nested block with \
           other properties and newlines should yield a port number";
@@ -153,18 +239,24 @@ TEST_F(ServerConfigTest, MultiWordProperties) {
     std::unique_ptr<ServerConfig> server_config
       = ParseString("server { \
                        echo \ntrue; \
-                       listen \n1234; \
+                       port \n1234; \
                        location /echo { \
                          action echo; \
                        } \
                      }");
+    bool buildCode = server_config->Build();
+
     std::vector<std::string> query = {"server", "location /echo", "action"};
     std::string action = "";
-    int ec = server_config->propertyLookUp(query, action);
+    bool ec = server_config->propertyLookUp(query, action);
 
-    EXPECT_EQ(0, ec)
-      << "Error value of 0 should be returned if property is found";
+    EXPECT_EQ(true, buildCode)
+      << "Build() should return true when given a valid config";
+    EXPECT_EQ(true, ec)
+      << "LookUp() should return true if property is found";
     EXPECT_EQ("echo", action)
       << "Requesting the action property should yield echo in this config.";
 }
+
+// TODO: unit test getChildBlock()
 
