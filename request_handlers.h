90d9322b513c92c7ec369973b68316d92a3a4165
Alexander Fong <alpfong@gmail.com>
2017-02-23 01:19:29 -0800

    Register request handlers in header
    In order for the handlers to be correctly registered, we need to
    call the macro REGISTER_REQUEST_HANDLER([handler_name]) in the .h
    
    Also, in order for the Registering template to work correctly with
    our ServerConfig member variable, we need to have a default
    constructor for it and the individual handlers.

--- request_handlers.h
+++ request_handlers.h
@@ -24,23 +24,20 @@ class RequestHandler {
     // Initializes the handler. Returns true if successful
     // uri_prefix is the value in the config file that this handler will run for.
     // config is the contents of the child block for this handler ONLY.
-    static RequestHandler* CreateByName(const std::string type);
+    static RequestHandler* CreateByName(const char* type);
 
     virtual bool init(const std::string& uri_prefix,
                       const NginxConfig& config) = 0;
 
     virtual Status handleRequest(const Request& request,
                                  Response* response) = 0;
-  protected:
-    std::string uri_prefix_;
-    ServerConfig config_;
 };
+
 
 // Notes:
 // * The trick here is that you can declare an object at file scope, but you
 //   can't do anything else, such as set a map key. But you can get around this
 //   by creating a class that does work in its constructor.
-//
 // * request_handler_builders must be a pointer. Otherwise, it won't necessarily
 //   exist when the RequestHandlerRegisterer constructor gets called.
 
@@ -48,7 +45,7 @@ extern std::map<std::string, RequestHandler* (*)(void)>* request_handler_builder
 template<typename T>
 class RequestHandlerRegisterer {
   public:
-    RequestHandlerRegisterer(const std::string type) {
+    RequestHandlerRegisterer(const std::string& type) {
       if (request_handler_builders == nullptr) {
         request_handler_builders = new std::map<std::string, RequestHandler* (*)(void)>;
       }
@@ -58,39 +55,57 @@ class RequestHandlerRegisterer {
       return new T;
     }
 };
-
 #define REGISTER_REQUEST_HANDLER(ClassName) \
   static RequestHandlerRegisterer<ClassName> ClassName##__registerer(#ClassName)
+
 
 // Echo the request that was received in the body of the response
 class EchoRequestHandler : public RequestHandler {
   public:
+    EchoRequestHandler() {}
+
     virtual bool init(const std::string& uri_prefix,
                       const NginxConfig& config);
 
     virtual Status handleRequest(const Request& request,
                                  Response* response);
+  private:
+    std::string uri_prefix_;
+    ServerConfig config_;
 };
+REGISTER_REQUEST_HANDLER(EchoRequestHandler);
 
 // Serve a up a file statically (without modifications)
 class StaticRequestHandler : public RequestHandler {
   public:
+    StaticRequestHandler() {}
+
     virtual bool init(const std::string& uri_prefix,
                       const NginxConfig& config);
 
     virtual Status handleRequest(const Request& request,
                                  Response* response);
+  private:
+    std::string uri_prefix_;
+    ServerConfig config_;
 };
+REGISTER_REQUEST_HANDLER(StaticRequestHandler);
 
 // Return a 404 error page
 class NotFoundRequestHandler : public RequestHandler {
   public:
+    NotFoundRequestHandler() {}
+
     virtual bool init(const std::string& uri_prefix,
                         const NginxConfig& config);
 
     virtual Status handleRequest(const Request& request,
                                  Response* response);
+  private:
+    std::string uri_prefix_;
+    ServerConfig config_;
 };
+REGISTER_REQUEST_HANDLER(NotFoundRequestHandler);
 
 #endif  // REQUEST_HANDLER_H
 
