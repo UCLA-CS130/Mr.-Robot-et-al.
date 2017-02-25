1a7c27d63ae44364aedc4d5df0564f820172942f
Frank Chen <frankchen1698@gmail.com>
2017-02-25 14:11:50 -0800

    Add build-essential to configure script

--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 sudo add-apt-repository -y ppa:jonathonf/python-3.6
 sudo apt-get -y update
+sudo apt-get install build-essential
 sudo apt-get -y install python3.6
 PATH=$(which python3.6)
 ln -s $PATH /usr/local/bin/python3.6
