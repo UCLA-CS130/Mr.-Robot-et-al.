9d966b41c58acad3e9bbb58e8a9812653216e4b5
Frank Chen <frankchen1698@gmail.com>
2017-02-25 13:49:30 -0800

    Update README on instructions for creating handlers
    * Create scripts to install lcov, httpie, and boost in configure script.

--- configure.sh
+++ configure.sh
@@ -3,3 +3,6 @@ sudo apt-get -y update
 sudo apt-get -y install python3.6
 PATH=$(which python3.6)
 ln -s $PATH /usr/local/bin/python3.6
+sudo apt-get install libboost-all-dev
+sudo apt-get install lcov
+sudo apt-get install httpie
