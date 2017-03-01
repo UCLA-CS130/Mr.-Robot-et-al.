0e80b16d0564e96ff8a35fc51e496f409107f7ac
Frank Chen <frankchen1698@gmail.com>
2017-02-28 16:48:07 -0800

    Add telnet before httpie in integration test for multithreading test

--- lightning_integration_test.py
+++ lightning_integration_test.py
@@ -10,12 +10,19 @@ print("\nSTART Lightning integration tests\n")
 # Try a background fork/thread
 server_process = subprocess.Popen(['./lightning', 'simple_config'])
 
+# Spawn a shell process to act as hanging http request
+telnet_request_command = "telnet localhost:2020/echo"
+telnet_request_proc = subprocess.Popen(telnet_request_command, stdout=subprocess.PIPE, shell=True)
+
 # TODO: Have intermediate logging throughout
 # TODO: Use Python unit test frameworks + logging libraries
 print('DEBUG: Lightning server started!')
 
-expected_response = b'GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: HTTPie/0.9.8\r\nAccept-Encoding: gzip, deflate\r\nAccept: */*\r\nConnection: keep-alive\r\n\r\n'
-actual_response = run(['http', 'localhost:8080'], stdout=subprocess.PIPE)
+# expected_response = b'GET / HTTP/1.1\r\nHost: localhost:2020\r\nUser-Agent: HTTPie/0.9.8\r\nAccept-Encoding: gzip, deflate\r\nAccept: */*\r\nConnection: keep-alive\r\n\r\n'
+expected_response = b'GET /echo HTTP/1.1\r\nHost: localhost:2020\r\nAccept-Encoding: gzip, deflate, compress\r\nAccept: */*\r\nUser-Agent: HTTPie/0.8.0\r\n\r\n'
+print("Expected Response\n")
+print(expected_response)
+actual_response = run(['http', 'localhost:2020/echo'], stdout=subprocess.PIPE)
 
 if (actual_response.returncode != 0):
     print('FAILED: httpie encountered an error')
@@ -23,8 +30,8 @@ if (actual_response.returncode != 0):
 if (actual_response.stdout != expected_response):
     print('FAILED: httpie received a non-matching echo response')
     print('Completed request: \n%s' % actual_response.stdout.decode('UTF-8'))
-
-print('SUCCESS: HTTPie request echo')
+else:
+    print('SUCCESS: HTTPie request echo; Multithreading successful!')
 
 # Terminate the server
 server_process.kill()
