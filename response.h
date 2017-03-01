92317af2285e12bf699754629cde4505a03292fa
binerys <bree.nery@gmail.com>
2017-03-01 01:41:44 -0800

    Extend Response class functionality: Response can be built from a raw response via Parse function

--- response.h
+++ response.h
@@ -9,22 +9,26 @@
 // Represents an HTTP response.
 //
 // Usage:
+// [1] Building a response from scratch
 //   Response r;
 //   r.SetStatus(RESPONSE_200);
 //   r.SetBody(...);
 //   return r.ToString();
+// [2] From a raw response
+//   auto response = Response::Parse(raw_response);
 //
 // Constructed by the RequestHandler,
 // after which the server should call ToString
 // to serialize.
 class Response {
  public:
+    // If adding new response codes, update GetResponseCode
     enum ResponseCode {
     OK = 200,
     NOT_FOUND = 404,
     FOUND = 302
   };
-
+  static std::unique_ptr<Response> Parse(const std::string& raw_response);
   void SetStatus(const ResponseCode response_code);
   void AddHeader(const std::string& header_name,
                  const std::string& header_value);
@@ -35,6 +39,7 @@ class Response {
 
   std::string ToString();
   std::string GetBody();
+  bool GetResponseCode(int code, Response::ResponseCode& rc); 
 
  private:
   std::map<int, std::string> reason_phrase_ = {
