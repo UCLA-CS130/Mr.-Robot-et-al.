d63510ad2430798622cd9943bae9097af162bad4
Ky-Cuong Huynh <kycodehuynh@gmail.com>
2017-03-08 18:27:42 -0800

    Remove commented code

--- lightning_integration_test.py
+++ lightning_integration_test.py
@@ -22,18 +22,14 @@ telnet_request_proc = Popen(telnet_request_command, stdout=PIPE, shell=True)
 # TODO: Use Python unit test frameworks + logging libraries
 print('DEBUG: Lightning server started!')
 
-# as the telnet is hanging, we will perform an echo request, which should still be 
-# handled by a separate handler in a new thread; it will still succeed and return 
+# as the telnet is hanging, we will perform an echo request, which should still be
+# handled by a separate handler in a new thread; it will still succeed and return
 # a response instantly.
 
 expected_response = b"""GET /echo HTTP/1.1\r\nHost: localhost:8080\r\n\
 Accept-Encoding: gzip, deflate, compress\r\n\
 Accept: */*\r\nUser-Agent: HTTPie/0.8.0\r\n\r\n"""
 actual_response = Popen(['http', 'localhost:8080/echo'], stdout=PIPE)
-
-# if (actual_response.returncode != 0):
-#     print('FAILED: httpie encountered an error')
-#     ec = 1;
 
 if (actual_response.communicate()[0].decode() != expected_response):
     print('FAILED: httpie received a non-matching echo response')
@@ -43,16 +39,12 @@ if (actual_response.communicate()[0].decode() != expected_response):
 else:
     print('SUCCESS: HTTPie request echo; Multithreading successful!')
 
-# Test proxy server 
+# Test proxy server
 print('DEBUG: Creating proxy server!')
 proxy_server_process = Popen(['./lightning', 'proxy_config'])
 
 expected_proxy_response = b'<!DOCTYPE html><html><head></head><body><h1>Hello World!</h1></body></html>'
 actual_proxy_response = Popen(['http', 'localhost:3030/reverse_proxy/static/index.html'], stdout=PIPE)
-
-# if (actual_proxy_response.returncode != 0):
-#     print('FAILED: proxy server encountered an error')
-#     ec = 1;
 
 if (actual_proxy_response.communicate()[0].decode() != expected_proxy_response):
     print('FAILED: proxy server received a non-matching proxy response')
